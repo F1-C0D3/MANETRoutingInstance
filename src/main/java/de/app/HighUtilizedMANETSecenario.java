5c8e5e231e4c35ec4b4ba07dc8f5afe1996ed0b1
Klement <klement.streit@gmail.com>
2021-08-23 09:14:16 +0200

    High Utilized Scenaro to generate DataRates of flows that slightly over utilize the network

--- src/main/java/de/app/HighUtilizedMANETSecenario.java
+++ src/main/java/de/app/HighUtilizedMANETSecenario.java
@@ -2,12 +2,10 @@ package de.app;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 import de.deterministic.algorithm.DijkstraShortesFlow;
-import de.jgraphlib.graph.algorithms.DijkstraShortestPath;
-import de.jgraphlib.graph.elements.Path;
 import de.jgraphlib.util.RandomNumbers;
-import de.jgraphlib.util.Triple;
 import de.jgraphlib.util.Tuple;
 import de.manetmodel.network.Flow;
 import de.manetmodel.network.Link;
@@ -15,134 +13,234 @@ import de.manetmodel.network.LinkQuality;
 import de.manetmodel.network.MANET;
 import de.manetmodel.network.Node;
 import de.manetmodel.network.unit.DataRate;
-import de.manetmodel.network.unit.DataUnit;
 import de.manetmodel.scenarios.Scenario;
 
 public class HighUtilizedMANETSecenario extends Scenario {
 
-	public HighUtilizedMANETSecenario(String individualName, int numFlows, int numNodes) {
-		super(individualName, numFlows, numNodes);
+	private enum DataType {
+		HD_Video, SD_Video, VoIP
 	}
 
-	public List<Flow<Node, Link<LinkQuality>, LinkQuality>> generateFlows(
-			MANET<Node, Link<LinkQuality>, LinkQuality, Flow<Node, Link<LinkQuality>, LinkQuality>> manet, int runs) {
-		DijkstraShortesFlow sp = new DijkstraShortesFlow(manet);
+	private List<Tuple<DataType, Long>> flowCharacteristics;
+
+	public HighUtilizedMANETSecenario(String individualName, int numFlows, int numNodes, int runs) {
+		super(individualName, numFlows, numNodes, runs);
+		this.flowCharacteristics = new ArrayList<Tuple<DataType, Long>>();
+	}
+
+	@Override
+	protected void generateSourceTargetPairs(int runs) {
 		List<Integer> exclusionList = new ArrayList<Integer>();
-		List<Triple<Integer, Integer, Integer>> flowSourceTargetPairs = new ArrayList<Triple<Integer, Integer, Integer>>();
 
 		for (int i = 0; i < (numFlows * 2); i++) {
 			int randomNodeId = RandomNumbers.getInstance(runs).getRandomNotInE(0, numNodes, exclusionList);
 			exclusionList.add(randomNodeId);
 		}
 
-		Triple<Integer, Integer, Integer> stTriple = null;
+		Tuple<Integer, Integer> stTriple = null;
 		for (int i = 0; i < exclusionList.size(); i++) {
 
 			if (i % 2 == 0) {
-				stTriple = new Triple<Integer, Integer, Integer>();
+				stTriple = new Tuple<Integer, Integer>();
 				stTriple.setFirst(exclusionList.get(i));
 			} else {
 				stTriple.setSecond(exclusionList.get(i));
 				flowSourceTargetPairs.add(stTriple);
 			}
-			stTriple.setThird(-1);
 		}
 
-		for (Triple<Integer, Integer, Integer> triple : flowSourceTargetPairs) {
+	}
+
+	Function<Tuple<Flow<Node, Link<LinkQuality>, LinkQuality>, LinkQuality>, Double> metric = (tuple) -> {
+		LinkQuality linkQuality = tuple.getSecond();
+		Flow<Node, Link<LinkQuality>, LinkQuality> reversePath = tuple.getFirst();
+
+		double cost = linkQuality.getReceptionPower();
+		return cost;
+
+	};
+
+	@Override
+	public List<Flow<Node, Link<LinkQuality>, LinkQuality>> generatePaths(
+			MANET<Node, Link<LinkQuality>, LinkQuality, Flow<Node, Link<LinkQuality>, LinkQuality>> manet, int runs,
+			DataRate step) {
+
+		DijkstraShortesFlow sp = new DijkstraShortesFlow(manet);
+
+		int index = 0;
+
+		for (Tuple<Integer, Integer> tuple : this.flowSourceTargetPairs) {
+
 			double random = RandomNumbers.getInstance(runs).getRandom(0d, 1d);
 			if (random >= 0 && random < 0.2) {
-				triple.setThird(1);
+				flowCharacteristics.add(new Tuple<HighUtilizedMANETSecenario.DataType, Long>(DataType.HD_Video, null));
+
 			} else if (random >= 0.2 && random < 0.55) {
-				triple.setThird(2);
+				flowCharacteristics.add(new Tuple<HighUtilizedMANETSecenario.DataType, Long>(DataType.SD_Video, null));
+
 			} else if (random >= 0.55 && random <= 1d) {
-				triple.setThird(3);
+				flowCharacteristics.add(new Tuple<HighUtilizedMANETSecenario.DataType, Long>(DataType.VoIP, null));
+
 			}
-			manet.addFlow(manet.getVertex(triple.getFirst()), manet.getVertex(triple.getSecond()), new DataRate());
+
+			manet.addFlow(manet.getVertex(tuple.getFirst()), manet.getVertex(tuple.getSecond()), new DataRate());
 		}
 
-		for (Flow<Node, Link<LinkQuality>, LinkQuality> f : manet.getFlows()) {
-			f = sp.compute(f, (Tuple<LinkQuality, Flow<Node, Link<LinkQuality>, LinkQuality>> q) -> {
-				return 1d;
-			});
+		initializeDataRateRelations(step);
 
+		for (Flow<Node, Link<LinkQuality>, LinkQuality> flow : manet.getFlows()) {
+
+			sp.compute(flow, metric);
 		}
 
-		DataRate step = new DataRate(1d, DataUnit.Type.megabit);
+		while (true) {
 
-		boolean thresholdReached = false;
-		while (!thresholdReached) {
-
-			if (thresholdReached)
+			if (manet.getOverUtilization().get() != 0L) {
+				manet.undeployFlows();
 				break;
+			}
+			manet.undeployFlows();
 
-			this.adaptDataRate(flowSourceTargetPairs, manet.getFlows(), step);
 			for (Flow<Node, Link<LinkQuality>, LinkQuality> f : manet.getFlows()) {
 
+				Long rate = flowCharacteristics.get(f.getID()).getSecond();
+
+				f.setDataRate(new DataRate(f.getDataRate().get() + rate));
+
+				manet.deployFlow(f);
+
+				if (manet.getOverUtilization().get() > 0)
+					f.setDataRate(new DataRate(f.getDataRate().get() - rate));
+
+				manet.undeployFlow(f);
+
+			}
+
+			for (Flow<Node, Link<LinkQuality>, LinkQuality> f : manet.getFlows()) {
 				manet.deployFlow(f);
 			}
-			if (manet.getOverUtilization().get() != 0L)
-				thresholdReached = true;
 
-			manet.undeployFlows();
 		}
+
 		for (Flow<Node, Link<LinkQuality>, LinkQuality> f : manet.getFlows()) {
 			f.clear();
+			System.out.println(f.toString());
 		}
 		return manet.getFlows();
 	}
 
-	private void adaptDataRate(List<Triple<Integer, Integer, Integer>> flowSourceTargetPairs,
-			List<Flow<Node, Link<LinkQuality>, LinkQuality>> flows, DataRate step) {
+	private void initializeDataRateRelations(DataRate step) {
 
 		int numFlowTypeOne = 0;
 		int numFlowTypeTwo = 0;
 		int numFlowTypeThree = 0;
 
-		for (Triple<Integer, Integer, Integer> triple : flowSourceTargetPairs) {
-			int flowType = triple.getThird();
+		for (Tuple<DataType, Long> tuple : flowCharacteristics) {
 
-			switch (flowType) {
+			DataType dataType = tuple.getFirst();
 
-			case 1:
+			switch (dataType) {
+
+			case HD_Video:
 				numFlowTypeOne++;
 				break;
-			case 2:
+			case SD_Video:
 				numFlowTypeTwo++;
 				break;
-			case 3:
+			case VoIP:
 				numFlowTypeThree++;
 				break;
 			default:
 				break;
 			}
 		}
-		int index = 0;
-		for (Triple<Integer, Integer, Integer> triple : flowSourceTargetPairs) {
-			int flowType = triple.getThird();
+
+		for (Tuple<DataType, Long> tuple : flowCharacteristics) {
+
+			DataType dataType = tuple.getFirst();
 			long amount = 0;
-			switch (flowType) {
 
-			case 1:
+			switch (dataType) {
+
+			case HD_Video:
+
 				amount = (long) (step.get() * 0.6);
-				amount = amount / numFlowTypeThree;
-				break;
-			case 2:
-				amount = (long) (step.get() * 0.3);
-				amount = amount / numFlowTypeThree;
+				tuple.setSecond(amount / numFlowTypeThree);
 
 				break;
-			case 3:
+			case SD_Video:
+
+				amount = (long) (step.get() * 0.3);
+				tuple.setSecond(amount / numFlowTypeThree);
+
+				break;
+			case VoIP:
+
 				amount = (long) (step.get() * 0.1);
-				amount = amount / numFlowTypeThree;
+				tuple.setSecond(amount / numFlowTypeThree);
+
 				break;
 			default:
 				break;
 			}
-			DataRate currentDataRate = flows.get(index).getDataRate();
-			flows.get(index).setDataRate(new DataRate(amount + currentDataRate.get()));
-			index++;
-		}
 
+		}
 	}
+
+//	private void adaptDataRate(List<Flow<Node, Link<LinkQuality>, LinkQuality>> flows, DataRate step) {
+//
+//		int numFlowTypeOne = 0;
+//		int numFlowTypeTwo = 0;
+//		int numFlowTypeThree = 0;
+//
+//		for (Triple<Integer, Integer, Integer> triple : flowSourceTargetPairs) {
+//			int flowType = triple.getThird();
+//
+//			switch (flowType) {
+//
+//			case 1:
+//				numFlowTypeOne++;
+//				break;
+//			case 2:
+//				numFlowTypeTwo++;
+//				break;
+//			case 3:
+//				numFlowTypeThree++;
+//				break;
+//			default:
+//				break;
+//			}
+//		}
+//		int index = 0;
+//		for (Triple<Integer, Integer, Integer> triple : flowSourceTargetPairs) {
+//
+//			int flowType = triple.getThird();
+//			long amount = 0;
+//
+//			switch (flowType) {
+//
+//			case 1:
+//				amount = (long) (step.get() * 0.6);
+//				amount = amount / numFlowTypeThree;
+//				break;
+//			case 2:
+//				amount = (long) (step.get() * 0.3);
+//				amount = amount / numFlowTypeThree;
+//
+//				break;
+//			case 3:
+//				amount = (long) (step.get() * 0.1);
+//				amount = amount / numFlowTypeThree;
+//				break;
+//			default:
+//				break;
+//			}
+//
+//			DataRate currentDataRate = flows.get(index).getDataRate();
+//			flows.get(index).setDataRate(new DataRate(amount + currentDataRate.get()));
+//			index++;
+//		}
+//
+//	}
 
 }
