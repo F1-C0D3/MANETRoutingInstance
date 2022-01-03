5bb8520d89b6818dd0fe02b6f5d210d12666a9b9
scenprogs <scenprogs@scenprogs-ThinkPad-T480s>
2022-01-03 16:19:01 +0100

    Include a main function for testing

--- src/main/java/de/heuristic/optimization/ACOOptimization.java
+++ src/main/java/de/heuristic/optimization/ACOOptimization.java
@@ -1,7 +1,10 @@
 package de.heuristic.optimization;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.function.Function;
+
+import javax.swing.SwingUtilities;
 
 import de.aco.alg.ACOProperties;
 import de.aco.alg.multipath.IndependentMultiPath;
@@ -10,12 +13,31 @@ import de.aco.ant.AntConsumer;
 import de.aco.ant.AntGroup;
 import de.aco.ant.evaluation.AntEvaluator;
 import de.aco.ant.evaluation.AntGroupEvaluator;
+import de.jgraphlib.generator.NetworkGraphGenerator;
+import de.jgraphlib.generator.NetworkGraphProperties;
+import de.jgraphlib.generator.GraphProperties.DoubleRange;
+import de.jgraphlib.generator.GraphProperties.IntRange;
+import de.jgraphlib.gui.VisualGraphApp;
 import de.jgraphlib.util.RandomNumbers;
+import de.manetmodel.evaluator.DoubleScope;
+import de.manetmodel.evaluator.ScalarLinkQualityEvaluator;
+import de.manetmodel.evaluator.ScoreOrder;
+import de.manetmodel.generator.OverUtilizedProblemProperties;
+import de.manetmodel.generator.OverUtilzedProblemGenerator;
+import de.manetmodel.gui.printer.LinkUtilizationPrinter;
+import de.manetmodel.mobilitymodel.PedestrianMobilityModel;
 import de.manetmodel.network.scalar.ScalarLinkQuality;
 import de.manetmodel.network.scalar.ScalarRadioFlow;
 import de.manetmodel.network.scalar.ScalarRadioLink;
 import de.manetmodel.network.scalar.ScalarRadioMANET;
+import de.manetmodel.network.scalar.ScalarRadioMANETSupplier;
+import de.manetmodel.network.scalar.ScalarRadioModel;
 import de.manetmodel.network.scalar.ScalarRadioNode;
+import de.manetmodel.units.DataRate;
+import de.manetmodel.units.Speed;
+import de.manetmodel.units.Unit;
+import de.manetmodel.units.Watt;
+import de.manetmodel.units.Speed.SpeedRange;
 import de.parallelism.Optimization;
 
 public class ACOOptimization extends Optimization<ScalarRadioMANET> {
@@ -121,4 +143,69 @@ public class ACOOptimization extends Optimization<ScalarRadioMANET> {
 
 		return null;
 	}
+	
+	public static void main(String args[]) throws InvocationTargetException, InterruptedException {
+		
+		/**************************************************************************************************************************************/
+		/* Prepare model */
+		
+		ScalarRadioModel radioModel = new ScalarRadioModel(
+				new Watt(0.002d), 
+				new Watt(1e-11), 1000d, 
+				2412000000d,
+				100d,
+				100d);
+		
+		PedestrianMobilityModel mobilityModel = new PedestrianMobilityModel(
+				new RandomNumbers(), 
+				new SpeedRange(0, 100, Unit.TimeSteps.second, Unit.Distance.meter), 
+				new Speed(50, Unit.Distance.meter, Unit.TimeSteps.second));
+		
+		ScalarLinkQualityEvaluator evaluator = new ScalarLinkQualityEvaluator(
+				new DoubleScope(0d, 10d), ScoreOrder.DESCENDING, radioModel, mobilityModel);
+		
+		ScalarRadioMANET manet = new ScalarRadioMANET(new ScalarRadioMANETSupplier().getNodeSupplier(),
+			new ScalarRadioMANETSupplier().getLinkSupplier(),
+			new ScalarRadioMANETSupplier().getLinkPropertySupplier(),
+			new ScalarRadioMANETSupplier().getFlowSupplier(),
+			radioModel, mobilityModel, evaluator);
+							
+		NetworkGraphProperties properties = new NetworkGraphProperties(
+				/* playground width */ 			1024,
+				/* playground height */ 		768, 
+				/* number of vertices */ 		new IntRange(100, 100),
+				/* distance between vertices */ new DoubleRange(50d, 100d),
+				/* edge distance */ 			new DoubleRange(100d, 100d));
+
+		NetworkGraphGenerator<ScalarRadioNode, ScalarRadioLink, ScalarLinkQuality> generator = 
+				new NetworkGraphGenerator<ScalarRadioNode, ScalarRadioLink, ScalarLinkQuality>(
+						manet, 
+						new ScalarRadioMANETSupplier().getLinkPropertySupplier(), 
+						new RandomNumbers());
+
+		generator.generate(properties);
+		
+		manet.initialize();
+							
+		/**************************************************************************************************************************************/
+		/* Setup problem & compute*/
+					
+		OverUtilzedProblemGenerator<ScalarRadioNode, ScalarRadioLink, ScalarLinkQuality, ScalarRadioFlow> overUtilizedProblemGenerator = 
+				new OverUtilzedProblemGenerator<ScalarRadioNode, ScalarRadioLink, ScalarLinkQuality, ScalarRadioFlow>(
+						manet, 
+						(ScalarLinkQuality w) -> { return w.getScore();});
+
+		OverUtilizedProblemProperties problemProperties = 
+				new OverUtilizedProblemProperties(
+						10, 10, 20, new DataRate(10), new DataRate(100), false, 5, new DataRate(10));
+	
+		manet.addFlows(overUtilizedProblemGenerator.compute(problemProperties));
+				
+		ACOOptimization aco = new ACOOptimization(manet, new RandomNumbers());		
+		aco.execute();		
+		
+		/**************************************************************************************************************************************/
+		/* Plot graph & solution */	
+		SwingUtilities.invokeAndWait(new VisualGraphApp<ScalarRadioNode, ScalarRadioLink, ScalarLinkQuality>(manet, new LinkUtilizationPrinter<ScalarRadioLink, ScalarLinkQuality>()));
+	}	
 }
